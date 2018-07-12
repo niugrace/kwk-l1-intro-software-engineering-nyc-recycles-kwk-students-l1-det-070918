6eb4dd83871d883ac1a8f5251e583e3ed8f7018c
niugrace <grace.niu.01@gmail.com>
2018-07-12 19:01:04 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -5,14 +5,14 @@
 # We haven't hard coded the hashes in the tests, 
 #they're dynamically brought in from the NYC Open Data API
 
-# first_recycle_bin =  {
-#   "address"=>"E 227 St/Bronx River Pkway",
-#   "site_type"=>"Subproperty",
-#   "longitude"=>"-73.864223918",
-#   "borough"=>"Bronx",
-#   "latitude"=>"40.890848989",
-#   "park_site_name"=>"227th St. Plgd"
-# }
+first_recycle_bin =  {
+   "address"=>"E 227 St/Bronx River Pkway",
+   "site_type"=>"Subproperty",
+   "longitude"=>"-73.864223918",
+   "borough"=>"Bronx",
+   "latitude"=>"40.890848989",
+   "park_site_name"=>"227th St. Plgd"
+}
 
 
 #1. Define a method `address`, that takes a hash as an argument.
@@ -32,3 +32,17 @@
 #5. Define a method called `key_and_data` that takes a hash as an argument.
 #This method should print out each key and value in the hash in a sentence like this: `The #{key} is #{value}`
 #You should use the `each` method to iterate over your hash to print out that sentence
+
+
+
+def address(first_recycle_bin)
+end
+
+def borough(second_recycle_bin)
+end
+
+def all_keys 
+
+  
+  
+  
